c0f9b6dba7c543781d7fcfb2e1453cb2e1329946
13virat <viratgupta1314@gmail.com>
2023-06-27 00:32:32 +0530

    nx

--- mychat/static/js/streams.js
+++ mychat/static/js/streams.js
@@ -1,18 +1,24 @@
 
 const APP_ID = 'c3dfe7dfb0b34716a26cdb0eaaa02475';
-const CHANNEL = 'main';
-const TOKEN = '007eJxTYFjU+0zDxWxa0UIR89BNUtpy975c+8r93NN+RcmTWYlVNzUUGJKNU9JSzVPSkgySjE3MDc0SjcySU5IMUhMTEw2MTMxNy6pnpjQEMjIczG1lZmSAQBCfhSE3MTOPgQEAboYgQw==';
-let UID;
+const CHANNEL = sessionStorage.getItem('room');
+const TOKEN = sessionStorage.getItem('token');
+let UID=Number(sessionStorage.getItem('UID'));
 
 const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 let localTracks = [];
 let remoteUsers = {};
 
 let joinAndDisplayLocalStream = async () => {
+  document.getElementById('room-name').innerText=CHANNEL
   client.on('user-published',handleUserJoined)
   client.on('user-left',handleUserLeft)
-  
-  UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
+  try{
+    await client.join(APP_ID, CHANNEL, TOKEN, UID);
+  }catch(error){
+    console.error(error)
+    window.open('/','_self')
+  }
+   
 
   localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
   let player = `<div class="video-container" id="user-container-${UID}">
